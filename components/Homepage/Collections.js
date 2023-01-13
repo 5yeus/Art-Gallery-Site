6afa0c275a3880bec300b3a81cf9ab3a55c00c4b
Silas  Owu <silas.owu@amalitech.com>
2023-01-13 15:41:43 -0800

    Pages components added

---
+++ components/Homepage/Collections.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Homepage.css"
+// import Artwall from "../../assets/images/gridimage.jpg"
+
+
+const Collections = () => {
+  return (
+    <div className='collections'>
+      <div class="collections-items">
+        1
+        {/* <h2>
+          YOUR DAY AT THE GALLERY
+        </h2>
+        <p>
+          Wander through our distinct collections
+          and find new insights about our artists. 
+          Dive into the details of their creative process.
+        </p> */}
+      </div>
+      
+      <div class="collections-items">
+        2{/* <img src={Artwall} alt="art-wall"/> */}
+      </div>
+      <div class="collections-items">3</div>
+      <div class="collections-items">4</div>
+      <div class="collections-items">5</div>
+    </div>
+  )
+}
+
+export default Collections
---
+++ components/Homepage/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Landingpage from './Landingpage'
+import Collections from './Collections'
+import footer from './Footer'
+
+
+
+const Homepage = () => {
+  return (
+    <div >
+      <Landingpage/>
+      <Collections/>
+
+    </div>
+  )
+}
+
+export default Homepage
